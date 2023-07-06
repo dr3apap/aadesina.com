4693e234bc41de9d82227f09e5be327667604b4c
Adebola Adesina <dr3apap@gmail.com>
2023-07-05 18:59:43 -0700

    Refactor:Change Demos component to use flex and wrap Demos with a parent
    Demos is going to be a carousel with a left and right button ffor scrolling

--- src/scripts/themeToggle.js
+++ src/scripts/themeToggle.js
@@ -2,7 +2,17 @@ const TOGGLE_FORM = document.querySelector("#theme-toggle");
 const TOGGLE_BUTTON = TOGGLE_FORM.querySelector("button");
 const TOGGLE_LABEL = TOGGLE_FORM.querySelector("#theme-toggle-label");
 const url = TOGGLE_FORM.action
+const navToggle = document.querySelector("div [aria-controls]");
+const navMenu = document.querySelector("nav ul");
+const bar = document.querySelector(".bar")
 
+function toggleNav(e) {
+        let barState = e.currentTarget.firstElementChild.getAttribute("aria-expanded") == "true" || false;
+        bar.setAttribute("aria-expanded", !barState);
+        navMenu.classList.toggle("show");
+
+    }
+navToggle.addEventListener("click", toggleNav);
 
 const toggleTheme = async () => {
   const themeState = await (await fetch(`${url}?${new URLSearchParams({client:true,})}`)).json();
