2bcd8e9b1c68b49b219029a13b72717ade967761
Adebola Adesina <dr3apap@gmail.com>
2023-07-14 11:17:19 -0700

    Fix:Add netlify adapter for ssr/hybrid output and rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,12 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-//import netlify from "@astrojs/netlify/edge-functions";
+import netlify from "@astrojs/netlify/edge-functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.aadesina.com",
   integrations: [mdx(), sitemap(), react(), tailwind()],
   output: "server",
-  //adapter: netlify()
+  adapter: netlify()
 });
