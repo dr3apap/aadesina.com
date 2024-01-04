8e816f02a104052013158d277f09422bdc213110
Adebola Adesina <adebola@aadesina.com>
2024-01-04 12:52:26 -0800

    Refactor:Refactor props to dynamic data

--- src/components/blog-categories/blog-categories.jsx
+++ src/components/blog-categories/blog-categories.jsx
@@ -1,30 +1,27 @@
-import React from 'react'
-import BlogCategory from '../blog-category/blog-category'
-import catIcons from './categories-icon.jsx'
-
-function BlogCategories({ posts }) {
-    return (
-        <div className="my-6">
-            <h2 className="text-fluid-2 la:text-fluid-3 text-text-2-accent mb-2 uppercase tracking-wide">
-                Categories
-            </h2>
-            <p>Here are some of my thougts, exploration, observations, and discovery. I hope the readers find informations and ideas that can be transform to reasonings of their own</p>
-            <ul
-                className="posts flex shrink-0  items-center justify-items-center flex-wrap gap-2 bg-surface-2 drop-shadow-md rounded-md my-6 p-2"
-                id="search-posts"
-            >
-                {Object.keys(catIcons).map((key, index) => {
-                    return (
-                        <BlogCategory
-                            key={`${index}-${key}`}
-                            title={key}
-                            Icon={catIcons[key]}
-                            posts={posts.length > 0 ? posts.filter((post) => post.category.name == key) : []}
-                        />
-                    )
-                })}
-            </ul>
-        </div>
-    )
-}
-export default BlogCategories
+//import React from 'react'
+//import BlogCategory from '../blog-category/blog-category'
+////import catIcons from './categories-icon.jsx'
+//
+//function BlogCategories({ postCategory }) {
+//    return (
+//        <div className="my-6">
+//            {/*<ul            className="posts flex shrink-0  items-center justify-items-center flex-wrap gap-2 bg-surface-2 drop-shadow-md rounded-md my-6 p-2"
+//            id="search-posts"
+//            >*/}
+//
+//            {/*{Object.keys(catIcons).map((key, index) => {*/}
+//            {/*return (*/}
+//            {...["frontEnd", "backEnd", "iot", "gameDesign", "systemDesign", "operatingSystem", "networking", "tools"].map((key) =>
+//                postCategory[key].map((postLink) =>
+//                    <BlogCategor
+//                        title={key} y
+//                        post={postLink}
+//                    />
+//                ))}
+//            <p className="text-fluid--2 text-brand">Sorry! there is no Post in <span className="italic text-text-2">{title}</span> yet, please check back later.</p>
+//
+//
+//        </div>
+//    )
+//}
+//export default BlogCategories
