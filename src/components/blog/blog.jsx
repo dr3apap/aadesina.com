4b9a1d4d6f84fc0c5b01125c3dd829aa423173da
Adebola Adesina <adebola@aadesina.com>
2024-01-04 11:54:39 -0800

    Refactor:Refactor blog components

---
+++ src/components/blog/blog.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import ContentBlock from '../content-block/content-block'
+import PostFilter from '../post-filter/post-filter.jsx'
+
+const shortDateFormatter = new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'short',
+})
+
+
+const getShortDate = (stamp) => shortDateFormatter.format(new Date(stamp))
+
+export function PostLink({ title, ...posts }) {
+    return (
+        <ul className="posts__render mt-3">
+            <h3 className="text-flud--1 uppercase text-text-1">{title}</h3>
+            {posts.selection.map((post) => {
+                const postUrl = 'aadesina.com'.toUpperCase()
+                return <li
+                    className="mb-2" key={post?._id}
+                >
+                    <a
+                        className="leading-tight decoration-4 decoration-brand text-brand"
+                        href={`/posts/${post?.slug.current}`}
+                    >
+                        {post?.title}
+                    </a>
+                    <p className="text-fluid--2">
+                        {`${post && postUrl} - ${getShortDate(post?.publishedAt)}`}
+                    </p>
+
+                </li>
+            })}
+        </ul>)
+
+}
+
+
+const postsByCategory = ["frontEnd", "backEnd", "iot", "operatingSystem", "gameDesign", "systemDesign", "tools"]
+
+export default function Blog({ postCategories, ...props }) {
+    const posts = postsByCategory.filter((category) => !!postCategories.data[category]
+    )
+    const mapPost = posts.map((category) => postCategories.data[category])
+    return <div className="max-w-full">
+        <ContentBlock type='blog' children={postCategories.data.blogHeading} />
+        <ContentBlock type='blog' children={postCategories.data.blogIntro} />
+        <div className="w-full flex justify-end">
+            <span className="flex gap-x-1">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                >
+                    <path d="M12.232 4.232a2.5 2.5 0 013.536 3.536l-1.225 1.224a.75.75 0 001.061 1.06l1.224-1.224a4 4 0 00-5.656-5.656l-3 3a4 4 0 00.225 5.865.75.75 0 00.977-1.138 2.5 2.5 0 01-.142-3.667l3-3z" />
+                    <path d="M11.603 7.963a.75.75 0 00-.977 1.138 2.5 2.5 0 01.142 3.667l-3 3a2.5 2.5 0 01-3.536-3.536l1.225-1.224a.75.75 0 00-1.061-1.06l-1.224 1.224a4 4 0 105.656 5.656l3-3a4 4 0 00-.225-5.865z" />
+                </svg>
+                <span>
+                    <a className="font-bold text-brand decoration-4 decoration-brand" href="/blog">
+                        permalink
+                    </a>
+                </span>
+            </span>
+        </div>
+        <PostFilter />
+        {mapPost.map((selection) => <PostLink key={selection.title} {...selection} />)}
+    </div>
+}
