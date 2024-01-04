8722c27d8d63c7471d3df5d9838b40406edbbb8d
Adebola Adesina <adebola@aadesina.com>
2024-01-04 13:29:36 -0800

    Refactor:Global config

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,11 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Adebola's Page";
-export const SITE_DESCRIPTION = "Adebola's Web Page";
-export const site = "www.aadesina.com";
 export const siteMetaData = {
-    mainPage:"Adebola's Page",
-    blog:"Adebola's Blog",
-    mainPageDes:"Adebola's Web Page",
-    blogDes:"Adebola's Blog Page",
-    siteUrl:"www.aadesina.com"
+  mainPage: "Adebola's Page",
+  blog: "Adebola's Blog",
+  mainPageDes: "Adebola's Web Page",
+  blogDes: "Adebola's Blog Page",
+  siteUrl: 'www.aadesina.com',
 }
+
