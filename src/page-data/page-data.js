17627797517efcb75b21af1fa9f39dd29d815863
Adebola Adesina <adebola@aadesina.com>
2024-01-04 12:12:03 -0800

    Feature:Add test data for pages

---
+++ src/page-data/page-data.js
@@ -0,0 +1,74 @@
+export const demosData = {
+  data: [
+    /* {
+        title:"Game Hub",
+        description:"A nice collections of online web games that can be play for fun and leizure. Play with online friends save your progress and chat live with other gamer and socialize",
+        link:"#",
+        skills:["css.svg", "react.svg", "tailwind.svg", "node.svg", "html.svg", "ts.svg", "javascript.svg"
+        ],
+        imageSrc:"game.jpg",
+  },
+   {
+        title:"Calendar",
+        description:"A program for days of the week, months of the year and personal memos that can be use to mark and memonize a particular event for a specific time, it also have journal, event and event reminder",
+        link:"",
+        skills:["css.svg", "react.svg", "tailwind.svg", "node.svg", "html.svg", "ts.svg","javascript.svg"
+        ],
+        imageSrc:"calendar.jpg",
+  },
+  */
+
+    {
+      title: '3D Animation',
+      description:
+        'Graphic display and manipulation is one of the intrigue things about the Web and most computer applications. Knowing the concept and ideas about it is more appealing',
+      link: '#',
+      skills: [
+        'css.svg',
+        'react.svg',
+        'tailwind.svg',
+        'node.svg',
+        'html.svg',
+        'ts.svg',
+        'javascript.svg',
+      ],
+      imageSrc: '3d.jpg',
+    },
+
+    {
+      title: 'Uptime Check',
+      description:
+        'This program allow dev-op/administrator or anyone with web asset web site or Api to track the status of the server, get notififcations for up; down and timeout state of the server ...',
+      link: '#',
+      skills: [
+        'css.svg',
+        'react.svg',
+        'tailwind.svg',
+        'node.svg',
+        'html.svg',
+        'ts.svg',
+        'javascript.svg',
+      ],
+      imageSrc: 'programming.jpg',
+    },
+  ],
+
+  feature: {
+    title: 'Bug Tracker',
+    description:
+      'A ticket/issues tracker for program describing the specific error(s) or issue(s) in details and allow developer or dev-op to respond to such error(s) or issue(s) while allowing updates and response to the ticket barrier',
+    link: '#',
+    skills: [
+      'css.svg',
+      'react.svg',
+      'tailwind.svg',
+      'node.svg',
+      'html.svg',
+      'ts.svg',
+      'javascript.svg',
+    ],
+    imageSrc: 'bug-tracker.png',
+  },
+}
+export const { demo } = demosData
+export const { feature } = demosData
