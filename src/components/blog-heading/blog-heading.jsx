bbb2dadd8340ef497f87d1a6b22d195cdf2e454f
Adebola Adesina <adebola@aadesina.com>
2024-01-04 12:57:30 -0800

    Refactor:Change heading components props to dynamic data

--- src/components/blog-heading/blog-heading.jsx
+++ src/components/blog-heading/blog-heading.jsx
@@ -1,25 +1,10 @@
 import React from 'react'
-function BlogHeading() {
-  return (
-    <div className="my-6">
-      <h1 className="text-fluid-2 la:text-fluid-3 uppercase tracking-wide text-text-2-accent mb-3">
-        sharing
-      </h1>
-      <div className="blog__intro__intro">
-        <p>
-          I love the dev commnunity, I appreciate open source and above all I
-          love sharing knowledges.
-        </p>
-        <p>
-          My Learning was predominantly open source as a self taught this is why
-          I'm giving back by sharing what I'm learning, exploring or discovered.
-        </p>
-        <p>
-          We all rise when we share our thougt and knowledges because no one is
-          an Island of <i>knowledge</i>.
-        </p>
-      </div>
-    </div>
-  )
+import BlogHeadingBlock from '../../components/content-block/content-block'
+function BlogHeading({ children }) {
+    return (
+        <div className="my-6">
+            <BlogHeaingBlock type='blBlogCatergoriesogHeading' children={children} />
+        </div>
+    )
 }
 export default BlogHeading
