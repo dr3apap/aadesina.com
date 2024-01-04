76bca7f985aca9c48ae008792019e3b0ca7f9c1b
Adebola Adesina <adebola@aadesina.com>
2024-01-04 13:41:29 -0800

    Refactor:Pages script Pages are now control by global script that switch each page on the client sides on request

--- src/scripts/pages.js
+++ src/scripts/pages.js
@@ -1,32 +1,89 @@
-function pageNavigator() {
-  const Navigator = Array.prototype.slice.call(
-    document.querySelectorAll('nav ul > li'),
-    2
-  )
-  const footer = Array.prototype.slice.call(
-    document.querySelectorAll('footer ul > li'),
-    3
-  )
+function pagesRouter() {
+  const pages = {}
+  let currentPage
+  let currentPageLink
+  const pageLinks = Array.from(
+    document.querySelectorAll('nav ul > li  a')
+  ).filter((el, i) => ![3, 4].includes(i))
 
-  //const blogPage = document.querySelector('#/blog')
-  const contactPage = document.querySelector('[data-tab-panel]')
-  console.log(contactPage)
-  let currentPageLink = ''
-  function currentPage(e) {
+  document.querySelectorAll('[data-tab-panel]').forEach((panel) => {
+    const path = panel.getAttribute('id')
+    pages[path] = panel
+  })
+  const footerNavigator = Array.from(
+    document.querySelectorAll('footer ul > li a')
+  ).filter((el, i) => [0, 1].includes(i))
+  footerNavigator.forEach((el) => el.addEventListener('click', footerSwitch))
+  const soloLink = document
+    .querySelector('#solo')
+    .addEventListener('click', (e) => {
+      e.preventDefault()
+      const [prevPage, nextPage] = pagesExtractor(pageLinks, e.currentTarget)
+      pageSwitch(prevPage, nextPage)
+    })
+
+  function footerSwitch(e) {
     e.preventDefault()
-    if (currentPageLink.length == 0) {
-      currentPageLink = e.currentTarget
-    } else {
-      if (currentPageLink.getAttribute('href').length > 0) {
-        currentPageLink.classList.remove('current')
-        currentPageLink.classList.add('hover')
-        currentPageLink = e.currentTarget
-      }
-      currentPageLink.classList.remove('hover')
-      currentPageLink.classList.add('current')
-      currentPageLink.href = `${contactPage.getAttribute('id').slice(1)}`
+    let footerPrev
+    if (e.currentTarget.dataset.tabCurrent == 'false') {
+      const [prevPage, nextPage] = pagesExtractor(pageLinks, e.currentTarget)
+      footerPrev = footerNavigator.find((el) => el.dataset.tabCurrent == 'true')
+      pageSwitch(prevPage, nextPage)
+      syncFooterLinks(footerPrev, e.currentTarget)
     }
+  }
+
+  function pagesExtractor(links, nextLink) {
+    let nextPage
+    let prevPage
+    nextPage = links.find(
+      (el) => el.getAttribute('href') == nextLink.getAttribute('href')
+    )
+    prevPage = links.find((el) => el.dataset.tabCurrent == 'true')
+    return [prevPage, nextPage]
+  }
+
+  function syncFooterLinks(prev, next) {
+    if (next && prev && typeof prev != 'object' && typeof next != 'object')
+      return
+    prev.dataset.tabCurrent = false
+    prev.querySelector('span').classList.remove('border-b-brand', 'text-text-1')
+    next.dataset.tabCurrent = true
+    next.querySelector('span').classList.add('border-b-brand', 'text-text-1')
+  }
+
+  function pageSwitch(prev, next) {
+    if (next && prev && typeof prev != 'object' && typeof next != 'object')
+      return
+    prev.dataset.tabCurrent = false
+    prev.querySelector('span').classList.remove('border-b-brand', 'text-text-1')
+    next.dataset.tabCurrent = true
+    next.querySelector('span').classList.add('border-b-brand', 'text-text-1')
+    pages[prev.getAttribute('href')].className = 'hidden'
+    pages[next.getAttribute('href')].className = 'block'
+  }
+
+  function pageNavigator(e) {
+    e.preventDefault()
+    pageLinks.forEach((el) => {
+      if (el.dataset.tabCurrent == 'true') currentPage = el
+    })
+    if (parseLink(e.currentTarget.getAttribute('href')) == currentPage) return
+    pageSwitch(currentPage, e.currentTarget)
+    if (e.currentTarget.getAttribute('href') != '/contact') {
+      const [footerPrev, footerNext] = pagesExtractor(
+        footerNavigator,
+        e.currentTarget
+      )
+      syncFooterLinks(footerPrev, footerNext)
+    }
+  }
+
+  for (const el of pageLinks) el.addEventListener('click', pageNavigator)
+
+  function parseLink(link) {
+    return link == '/home' || link == '/' ? '/' : link.slice(1)
   }
 }
 
-document.addEventListener('DOMContentLoaded', pageNavigator);
+document.addEventListener('DOMContentLoaded', pagesRouter)
