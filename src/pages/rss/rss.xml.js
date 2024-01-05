33a977ffe6c1054955780ae60637f41f899de689
Adebola Adesina <adebola@aadesina.com>
2024-01-05 13:56:49 -0800

    fix:Rss data

--- src/pages/rss/rss.xml.js
+++ src/pages/rss/rss.xml.js
@@ -6,16 +6,17 @@ const siteConfig = rssData.config || {}
 const allTags = rssData.tags || []
 const posts = rssData.posts || []
 const allCategory = rssData.category || []
-
+const categoryName = posts.find(post => post.category !== null).category.name
 const metadata = {
   url: siteConfig?.rss?.url || 'https://aadesina.com/',
   title: siteConfig?.rss?.title || 'Adebola Adesina',
-  subtitle: siteConfig?.rss?.subtitle || 'Posts from Adebola',
-  description:
-  siteConfig?.rss?.description || 'The RSS feed for posts from Adebola Adesina',
-  author: siteConfig?.character?.name || 'Adebola Adesina',
+  subtitle:siteConfig?.rss?.subtitle || 'Posts from Adebola',
+  description:siteConfig?.rss?.description || 'The RSS feed for posts from Adebola Adesina',
+  author: siteConfig?.character || '',
   email: 'rss@aadesina.com',
+  catergory:categoryName,
 }
+
 
 const allPosts = posts.length > 0? posts.map((post) => ({
   ...post,
