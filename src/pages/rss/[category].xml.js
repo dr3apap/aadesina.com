ad5ea0d6236367622e9c58d517dfd2120dbb9c4e
Adebola Adesina <adebola@aadesina.com>
2024-01-10 10:52:01 -0800

    refactor:Add pages routes by post category to rss feeds

--- src/pages/rss/[category].xml.js
+++ src/pages/rss/[category].xml.js
@@ -1,37 +1,40 @@
 import { getRssData } from '../../constants/queries';
 import { genRssMarkup } from './_html-generator';
+import { getPostByCategory } from '../../constants/queries';
+import { getAllCategory } from '../../constants/queries';
 
 const rssData = await getRssData();
-const siteConfig = rssData?.Config ||{};
-const allTags = rssData?.tags || [];
-let posts = rssData?.posts  || [];
-const allCategory = rssData?.category || [];
+const siteConfig = rssData.config || {};
+const postByCategory = await getPostByCategory();
+const categories = await getAllCategory();
+const postByCat = categories.map(category => postByCategory[0][category.name]).filter(el => el != null)
 export function getStaticPaths() {
-const categoryPaths = allCategory.length  > 0? allCategory.map((category) => {
-          return { params: { category: category.name.toLowerCase() },  }
-  }):[]
+const categoryPaths = postByCat.length > 0 ? postByCat.map(post => {
+          return { 
+              params: { category: post.title.toLowerCase()}, 
+          }
+           }):[]
   return categoryPaths
 };
 
 export const get = ({ params, request }) => {
   const metadata = {
     url: siteConfig?.rss?.url || 'https://aadesina.com/',
-    title: siteConfig?.rss?.title || 'https://aadesina.com/',
+    title: siteConfig?.rss?.title || `${params.category} post from Adebola Adesina`,
     subtitle: siteConfig?.rss?.subtitle || 'Posts from Adebola',
     description: `The RSS feed for ${params.category} posts from Adebola Adesina`,
-    author: siteConfig.character,
+    author: siteConfig?.character || {},
     email: 'rss@aadesina.com',
     category: params.category,
   }
-   posts = posts.length > 0? posts.filter(post => {
-    for(const postTag of allTags) {
-    return postTag.title && post.tags && post.tags.length > 0 && post.tags.find(tag => {
-      return tag.title !== null && tag.title.toLowerCase() === postTag.title.toLowerCase()
-    })}
-  }).map((post) => ({
-    ...post,
-    url: `${metadata.url}posts/${post.slug.current}`,
-  })).sort((a, b) => {
+   const posts = postByCat.length > 0? postByCat.map((category) => {
+        return category.title.toLowerCase() == params.category && 
+        category.selection.map(post => ({
+        ...post,
+        url: `${metadata.url}dr3papa/${post.slug.current}`,
+        }))
+
+   })[0].sort((a, b) => {
     const dateA = new Date(a.publishedAt)
     const dateB = new Date(b.publishedAt)
     return dateB - dateA
