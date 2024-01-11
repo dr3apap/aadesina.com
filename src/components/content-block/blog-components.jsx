27831a9e7b841da5255d7eb35efbdfe2369a4ad3
Adebola Adesina <adebola@aadesina.com>
2024-01-11 14:10:59 -0800

    styles:Renam accent text color

--- src/components/content-block/blog-components.jsx
+++ src/components/content-block/blog-components.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 const blogComponents = {
 
     h1({ node, children, ...props }) {
-        return <h1 className="text-fluid-2 la:text-fluid-3 uppercase tracking-wide text-text-2-accent mb-3" {...props}>
+        return <h1 className="text-fluid-2 la:text-fluid-3 uppercase tracking-wide text-accent-text-2 mb-3" {...props}>
             {children}
         </h1>
     },
 
     h2({ node, children, ...props }) {
-        return <h2 className="text-fluid-2 la:text-fluid-3 text-text-2-accent mb-2 uppercase tracking-wide" {...props}>{children}
+        return <h2 className="text-fluid-2 la:text-fluid-3 text-accent-text-2 mb-2 uppercase tracking-wide" {...props}>{children}
         </h2>
     },
 
