87f4ed0161eea6957b8f66048f1010c721802b89
Adebola Adesina <adebola@aadesina.com>
2024-01-21 18:57:14 -0800

    refactor:Demos components

--- src/components/demo/demos.jsx
+++ src/components/demo/demos.jsx
@@ -3,31 +3,35 @@ import DemoCard from '../demo-card/demo-card';
 import ContentBlock from '../../components/content-block/content-block'
 import Feature from '../../components/feature-demo/feature-demo'
 function Demos({ sectionIntro, demos, feature }) {
-    return (<section id="demos" tabIndex="-1" className="mt-20 p-0 space-y-8">
+    return (<section id="demos" tabIndex="-1" className="mt-20 p-0 space-y-8 ">
         <div>
             <ContentBlock type='demo' children={sectionIntro} />
         </div>
         <Feature {...feature[0]} />
         {/* Carousel */}
-        <div className="demos-wrapper relative">
+        <div className="demos-carousel relative">
             <div
-                className="demos-wrapper--slider-wrapper overflow-hidden"
+                className="demos-carousel--inner overflow-hidden"
             > {/*Decoy for overflow so that slide button can align with slider */}
 
                 {/* Demos cards wrapper */}
-
-                <div className="demos-slider inline-flex items-start gap-x-2 touch-none transition duration-150 ">
+                <ul className="demos-carousel--slider touch-none">
                     {/* Demos card */}
-                    {demos.map((demo) => (<DemoCard key={demo
-                        ._id}  {...demo} />
-                    ))}
+                    {demos.map((demo) =>
+                    (< DemoCard key={
+                        demo
+                            ._id
+                    }  {...demo} />
 
-                </div>{/* carousel slider*/}
+                    ))}
+                </ul>{/* carousel slider*/}
             </div>{/*carousel inner for hidden overflow */}
             <div className="hidden lg:block">
-                <button className="demos-slider--ctl left-[-1.75rem] hidden">&#10094;</button>
-                <button className="demos-slider--ctl right-[-1.75rem]">&#10095;</button>
+                <button className="demos-carousel--slider__cta left-[-1.75rem]" data-demo-ctl="left">&#10094;</button>
+                <button className="demos-carousel--slider__cta right-[-1.75rem]" data-demo-ctl="right">&#10095;</button>
             </div> {/*slider control button */}
+
+
         </div> {/* Carousel end*/}
     </section>); {/*Demos section end */ }
 
